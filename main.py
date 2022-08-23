e7cd38bc2267168b95cd829e52658818bfc32beb
Amneet Kaur <kauramneet986@gmail.com>
2022-08-23 13:39:18 -0400

    add new list that tells us positioning

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
+# ..........create a program from given lists for putting odd even numbers and make a new list from that elements.....
+
+# l1 = [3, 6, 9, 12, 15, 18, 21]
+# l2 = [4, 8, 12, 16, 20, 24, 28,21,27,31]
+# l3 = []
+# for i in l1:
+#     if (i  % 2 ) != 0:
+#         l3.append(i)
+# for i in l2:
+#     if(i % 2)== 0:
+#         l3.append(i)
+
+# print(l3)
+
+
+
+# .........Create a list by picking an odd-index items from the first list and even index items from the second......
 
 l1 = [3, 6, 9, 12, 15, 18, 21]
-l2 = [4, 8, 12, 16, 20, 24, 28,21,27,31]
-l3 = []
-for i in l1:
-    if (i  % 2 ) != 0:
-        l3.append(i)
-for i in l2:
-    if(i % 2)== 0:
-        l3.append(i)
+l2 = [4, 8, 12, 16, 20, 24, 28]
+l3 = l1[1::2]
+print(f'Element at odd-index positions from list one \n {l3}')
+l4 = l2[0::2]
+print(f'Element at even-index positions from list second \n {l4}')
 
-print(l3)
+a = l3 + l4
+print(f'Printing Final third list \n {a}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+        
